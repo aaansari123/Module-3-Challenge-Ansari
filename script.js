63616b8c53c7d100681de42ef4abbf1d5af34859
aaansari123 <asadali1029@gmail.com>
2022-07-21 16:13:20 -0400

    added functionality to create a password that satisfies the challenge specifications

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ var spclChar = " !\"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~"
 var lowerLetters = "abcdefghijklmnopqrstuvwxyz"
 var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var nums = "0123456789"
+var randList = "";
+
 
 function generatePassword() {
-  var password = 'password';
+  var password = '';
   // TODO: add code to generate the password here
 
   return password;
@@ -14,10 +16,12 @@ function generatePassword() {
 
 // Write password to the #password input
 function writePassword() {
+    //reset megalist to empty so that previous conditions dont carry over.
+  randList = "";
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
   var length = prompt("Enter a password length between 8 - 128 characters in length");
-  if ((length < 8 ) || (length > 128)){
+  if ((length <= 8 ) || (length >= 128)){
     alert("This is an invalid length. Try again");
     return;
   } else{
@@ -25,8 +29,52 @@ function writePassword() {
     var hasLower = confirm("would you like lower case letters?");
     var hasSpc = confirm("would you like special characters?");
     var hasNums = confirm("would you like numbers?");
-  }
 
+    if (!hasUpper && !hasLower && !hasNums && !hasSpc){
+        alert("Please pick at least one type of character. Try again");
+        return;
+    }
+    //checks if upper is required, adds one upper case letter, adds upper case to our full random list
+    if(hasUpper){
+        var randUpper = Math.floor(Math.random() * upperLetters.length);
+        password = password.concat(upperLetters.charAt(randUpper));
+        length--;
+        randList += upperLetters;
+
+    }
+    //checks if lower is required, adds one lower case letter, adds lower case to our full random list
+
+    if(hasLower){
+        var randLower = Math.floor(Math.random() * lowerLetters.length);
+        password = password.concat(lowerLetters.charAt(randLower));
+        length--;
+        randList += lowerLetters;
+
+    }
+    //checks if special is required, adds one special case letter, adds special case to our full random list
+    if(hasSpc){
+        var randSpc = Math.floor(Math.random() * spclChar.length);
+        password = password.concat(spclChar.charAt(randSpc));
+        length--;
+        randList += spclChar;
+
+    }
+    //checks if numbers is required, adds number, adds numbers to our full random list
+    if(hasNums){
+        var randNum = Math.floor(Math.random() * nums.length);
+        password = password.concat(nums.charAt(randNum));
+        length--;
+        randList += nums;
+
+    }
+    //fills out the rest of our password with the appropriate types of values from our megalist
+    while (length > 0){
+        length--;
+        var randChar = Math.floor(Math.random() * randList.length)
+        password = password.concat(randList.charAt(randChar));
+        
+    }
+  }
   passwordText.value = password;
 }
 
