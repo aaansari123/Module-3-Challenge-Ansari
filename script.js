666d0da6b2c26cc2fb56bbf0138f237216636de5
aaansari123 <asadali1029@gmail.com>
2022-07-21 15:37:04 -0400

    added prompts for user preference

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
+var spclChar = " !\"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~"
+var lowerLetters = "abcdefghijklmnopqrstuvwxyz"
+var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var nums = "0123456789"
 
 function generatePassword() {
   var password = 'password';
@@ -12,6 +16,16 @@ function generatePassword() {
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
+  var length = prompt("Enter a password length between 8 - 128 characters in length");
+  if ((length < 8 ) || (length > 128)){
+    alert("This is an invalid length. Try again");
+    return;
+  } else{
+    var hasUpper = confirm("would you like upper case letters?");
+    var hasLower = confirm("would you like lower case letters?");
+    var hasSpc = confirm("would you like special characters?");
+    var hasNums = confirm("would you like numbers?");
+  }
 
   passwordText.value = password;
 }
